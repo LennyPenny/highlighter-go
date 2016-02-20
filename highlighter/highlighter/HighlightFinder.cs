d19cf3feb66e8c54c98a55c1ea35a32ed2af18a6
Lennart Bernhardt <l.bernhardt@live.de>
2016-02-20 23:59:44 +0100

    added multkill finder

---
+++ highlighter/highlighter/HighlightFinder.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using DemoInfo;
+
+namespace highlighter
+{
+    class HighlightFinder
+    {
+        private SortedDictionary<int, string> _highlights;
+
+        private DemoParser _parser;
+
+        public string Path { get; private set; }
+
+        public HighlightFinder(string path)
+        {
+            Path = path;
+            _parser = new DemoParser(File.OpenRead(path));
+
+            _parser.ParseHeader();
+
+            _highlights = new SortedDictionary<int, string>();
+
+            SetupHighlightHandlers();
+        }
+
+        private void SetupHighlightHandlers()
+        {
+            //multikill finder (finds kills in quick succession)
+            {
+                var counter = 0; //keeps track of kills in quick succession
+                var timeLimit = (float)Program.Settings.multiKill.timeLimit; //how many seconds until a multkill expires
+                var start = 0; //tick where the mutlkill started
+                var lastKill = .0f; //time the last kill of the multikill happened
+
+                _parser.RoundStart += (s, e) =>
+                {
+                    counter = 0;
+                    start = 0;
+                    lastKill = .0f;
+                };
+
+                _parser.PlayerKilled += (s, e) =>
+                {
+                    if (e.Killer?.SteamID != (long) Program.Settings.playerID)
+                        return;
+
+                    if (_parser.CurrentTime - lastKill <= timeLimit || counter == 0)
+                    {
+                        counter++;
+                        lastKill = _parser.CurrentTime;
+
+                        if (counter == 1)
+                            start = _parser.IngameTick;
+                    }
+                };
+
+                _parser.TickDone += (s, e) =>
+                {
+                    if (lastKill == .0f)
+                        return;
+
+                    if (_parser.CurrentTime - lastKill > timeLimit)
+                    {
+                        _highlights.Add(_parser.IngameTick, $"{counter} kills in quick succession");
+
+                        counter = 0;
+                        start = 0;
+                        lastKill = .0f;
+                    }
+
+                };
+            }
+        }
+
+        public async Task<SortedDictionary<int, string>> GetHighlights()
+        {
+            _parser.ParseToEnd();
+            _parser.Dispose();
+
+            return _highlights;
+        }
+    }
+}
--- highlighter/highlighter/Program.cs
+++ highlighter/highlighter/Program.cs
@@ -1,15 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting.Channels;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace highlighter
 {
-    class Program
+    public class Program
     {
+        public static dynamic Settings;
+
         static void Main(string[] args)
         {
+            Settings = JsonConvert.DeserializeObject(File.ReadAllText("settings.json"));
+
+            var results = new Dictionary<string, SortedDictionary<int, string>>();
+
+            var demos = new List<HighlightFinder>();
+
+            if (args.Length > 0)
+            {
+                foreach (var path in args)
+                {
+                    demos.Add(new HighlightFinder(path));
+                }
+            }
+            else
+            {
+                foreach (var path in Directory.GetFiles(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "*.dem"))
+                {
+                    demos.Add(new HighlightFinder(path));
+                }
+            }
+
+            var highlightTasks = new List<Task>();
+            foreach (var demo in demos)
+            {
+                highlightTasks.Add(Task.Run(async () =>
+                {
+                    results.Add(demo.Path, await demo.GetHighlights());
+                }));
+            }
+
+            Task.WaitAll(highlightTasks.ToArray());
         }
     }
 }
